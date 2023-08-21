8f7fe3b5ffcc25e99b615b68c3a51cb87bae72fa
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2023-08-21 17:38:30 +0500

    change text

--- src/components/about-app/index.jsx
+++ src/components/about-app/index.jsx
@@ -18,7 +18,7 @@ const AboutApp = () => {
             <Link className="about-app__link" to="https://tahrirlovchi.uz">
               Tahrirlovchi.uz
             </Link>{" "}
-            – so‘z va matnlarni lotin-kirill, kirill-lotin garafikasiga to‘g‘ri
+            – so‘z va matnlarni lotin-kirill, kirill-lotin grafikasiga to‘g‘ri
             o‘girish hamda tekshirish uchun yuborilgan matnni ilmo qoidalari
             asosida tahlil qilish va matnda xato yozilgan so‘zlarni ajratish,
             shuningdek o‘zbek tilining izohli va imlo lug‘ati maʼlumotlar
@@ -27,14 +27,15 @@ const AboutApp = () => {
           <br />
           <br />
           <p className="about-app__text">
-            Mazkur loyiha O‘zbekiston Respublikasi Prezidentining
-            “Mamlakatimizda o‘zbek tilini yana-da rivojlantirish va til
-            siyosatini takomillashtirish chora-tadbirlari to‘g‘risida” 2020-yil
-            20-oktabrdagi PF-6084-son Farmoni ijrosi doirasida Axborot
-            texnologiyalari va kommunikatsiyalarini rivojlantirish vazirligi
-            tizimidagi “Yagona integrator UZINFOCOM” MChJ tomonidan O‘zbek
-            tilini rivojlantirish jamg‘armasi mablag‘lari hisobidan amalga
-            oshirilgan.
+            Loyiha O‘zbekiston Respublikasi Prezidentining “Mamlakatimizda
+            o‘zbek tilini yana-da rivojlantirish va til siyosatini
+            takomillashtirish chora-tadbirlari to‘g‘risida” 2020-yil
+            20-oktabrdagi PF-6084-son Farmoni ijrosi doirasida O‘zbekiston
+            Respublikasi Vazirlar Mahkamasi Davlat tilini rivojlantirish
+            departamenti, O‘zbek tilini rivojlantirish jamg‘armasi hamda Raqamli
+            texnologiyalar vazirligi tizimidagi “Yagona integrator UZINFOCOM”
+            MChJ tomonidan O‘zbek tilini rivojlantirish jamg‘armasi mablag‘lari
+            hisobidan amalga oshirilgan.
           </p>
           <br />
           <br />
