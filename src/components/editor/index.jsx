1c6431a71c18a07c0aacdc2e91903349d615e8e3
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2023-08-22 12:15:34 +0500

    add star

--- src/components/editor/index.jsx
+++ src/components/editor/index.jsx
@@ -1,11 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import { TranslateIcon } from "../icons";
 import EditorContent from "./editor-content";
 import "./editor.scss";
+import { updateLang } from "../../store/reducer/editor-slice";
 
 const EditorContainer = () => {
+  const { isLatin } = useSelector((store) => store.editorState);
+  const dispatch = useDispatch();
+
   return (
     <div className="container editor">
       <EditorContent />
       <EditorContent pos={"right"} />
+      <div
+        className="button-container"
+        onClick={() => dispatch(updateLang(!isLatin))}
+      >
+        <div className="editor-button">
+          <TranslateIcon />
+        </div>
+      </div>
     </div>
   );
 };
