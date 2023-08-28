28f7cb1d5daca0fbeee2ab88afbfc5de0a3ec372
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2023-08-28 17:43:03 +0500

    make responsive

--- src/components/dictionary/dictionary-type/index.jsx
+++ src/components/dictionary/dictionary-type/index.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useGetWordTypesQuery } from "../../../store/api";
 import { queryMaker, queryStringify } from "../../../utils/helpers";
+import useOnClickOutside from "../../../hooks/useOnClickOutside";
 
 const DictionaryType = () => {
   const [open, setOpen] = useState(false);
+  const typeRef = useRef();
   const navigate = useNavigate();
 
   const { search } = useParams();
@@ -12,6 +14,7 @@ const DictionaryType = () => {
   const { type } = queryObj;
 
   const { data: types, isLoading } = useGetWordTypesQuery();
+  useOnClickOutside(typeRef, () => setOpen(false));
 
   const curType = (types || []).find((el) => el.id === (type || 1));
 
@@ -33,7 +36,7 @@ const DictionaryType = () => {
         {curType?.title}
       </button>
       {open && (
-        <ul className="dictionary-type__list">
+        <ul className="dictionary-type__list" ref={typeRef}>
           {types?.map((type) => (
             <li className="dictionary-type__item" key={type.id}>
               <button
