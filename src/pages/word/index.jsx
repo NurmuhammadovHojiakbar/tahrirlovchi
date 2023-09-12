5ca03993024e84036b76aaa96c6a5faddf179ccd
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2023-09-12 15:02:07 +0500

    add keywords

--- src/pages/word/index.jsx
+++ src/pages/word/index.jsx
@@ -2,12 +2,16 @@ import Page from "../../layout/page";
 import Container from "../../layout/container";
 import WordDescription from "../../components/word-description";
 import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const Word = () => {
   const { word } = useParams();
 
   return (
     <Page title={`${word} - Tahrirlovchi`}>
+      <Helmet>
+        <meta name="keywords" content={word} />
+      </Helmet>
       <section className="word">
         <Container style={{ background: "#eefff5", padding: "54px 0 74px" }}>
           <WordDescription />
